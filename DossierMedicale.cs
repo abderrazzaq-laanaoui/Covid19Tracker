d0007488e12a55934e172910fcbe3217148b21cc
abderrezzaq laanoui <laanoui.fst@uhp.ac.ma>
2020-12-15 12:49:58 +0100

    Implementation de la diagram class

---
+++ DossierMedicale.cs
@@ -0,0 +1,12 @@
+// File:    DossierMedicale.cs
+// Author:  ABDERRAZZAQ LAANOUI
+// Created: 03 December 2020 02:34:33
+// Purpose: Definition of Class DossierMedicale
+
+using System;
+using System.Collections.Generic;
+
+public class DossierMedicale
+{
+
+}
---
+++ Laboratoire.cs
@@ -0,0 +1,20 @@
+// File:    Laboratoire.cs
+// Author:  ABDO
+// Created: 03 December 2020 02:34:31
+// Purpose: Definition of Class Laboratoire
+
+using System;
+
+public class Laboratoire : Etablissement
+{
+   public bool TestPCR()
+   {
+      throw new NotImplementedException();
+   }
+   
+   public void EnvoyerResutat()
+   {
+      throw new NotImplementedException();
+   }
+
+}
