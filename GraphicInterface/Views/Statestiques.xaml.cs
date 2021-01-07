dc780e0dc090fd36cbd48e9360107bd7ada1eccf
abderrezzaq laanoui <laanoui.fst@uhp.ac.ma>
2021-01-07 21:37:14 +0100

    Ajouter graphe pour les regions

--- GraphicInterface/Views/Statestiques.xaml.cs
+++ GraphicInterface/Views/Statestiques.xaml.cs
@@ -3,6 +3,7 @@ using System.Windows.Controls;
 using System.Windows.Media;
 using LiveCharts;
 using LiveCharts.Wpf;
+using System.Linq;
 
 namespace GraphicInterface.Views
 {
@@ -14,6 +15,10 @@ namespace GraphicInterface.Views
         public SeriesCollection SeriesCollection { get; set; }
         public string[] Labels { get; set; }
         public Func<double, string> YFormatter { get; set; }
+        //---
+        public SeriesCollection SeriesCollection2 { get; set; }
+        public string[] Labels2 { get; set; }
+        public Func<double, string> Formatter2 { get; set; }
         public Statestiques()
         {
             InitializeComponent();
@@ -22,39 +27,52 @@ namespace GraphicInterface.Views
                 new LineSeries
                 {
                     Title = "Series 1",
-                    Values = new ChartValues<double> { 4, 6, 5, 2 ,4 }
+                    Values = new ChartValues<double> { 4, 6, 5, 2 ,4 },
+                    PointGeometry = DefaultGeometries.Diamond,
+                    PointGeometrySize = 10
                 },
                 new LineSeries
                 {
                     Title = "Series 2",
                     Values = new ChartValues<double> { 6, 7, 3, 4 ,6 },
-                    PointGeometry = null
+                    PointGeometry = DefaultGeometries.Square,
                 },
                 new LineSeries
                 {
                     Title = "Series 3",
                     Values = new ChartValues<double> { 4,2,7,2,7 },
                     PointGeometry = DefaultGeometries.Square,
-                    PointGeometrySize = 15
+                    PointGeometrySize = 1
                 }
             };
 
             Labels = new[] { "Jan", "Feb", "Mar", "Apr", "May" };
-            YFormatter = value => value.ToString("C");
+             
+            //________
 
-            //modifying the series collection will animate and update the chart
-            SeriesCollection.Add(new LineSeries
+            SeriesCollection2 = new SeriesCollection
             {
-                Title = "Series 4",
-                Values = new ChartValues<double> { 5, 3, 2, 4 },
-                LineSmoothness = 0, //0: straight lines, 1: really smooth lines
-                PointGeometry = Geometry.Parse("m 25 70.36218 20 -28 -20 22 -8 -6 z"),
-                PointGeometrySize = 50,
-                PointForeground = Brushes.Gray
-            });
+                new ColumnSeries
+                {
+                    Title = "Guerés",
+                    Fill = Brushes.Green,
+                    Values = new ChartValues<double> { 12, 30, 49, 60 , 123, 210, 160, 80,92, 71, 160, 94}
+                },
+                new ColumnSeries
+                {
+                    Title = "Infectés",
+                    Fill = Brushes.Red,
+                    Values = new ChartValues<double> { 10, 50, 39, 50 , 223, 120, 60, 98,112, 100, 60, 54}
+                }
+                
+            };
 
-            //modifying any series values will also animate and update the chart
-            SeriesCollection[3].Values.Add(5d);
+
+            //also adding values updates and animates the chart automatically
+            /**/
+            Labels2 = new[] { "Tangier_Tetouan_AlHociema", "Oriental", "Fez_Meknes", "Rabat_Sale_Kenitra", "BeniMellal_Khenifra",
+                "CasaBlanca_Settat", "Marrakesh_Safi", "Draa_Tafilalt", "Sous_Massa","Guelmim_OuedNoun","Laayoune_SakiaElHamra","Dakhla_OuedEddahab" };
+
 
             DataContext = this;
         }
