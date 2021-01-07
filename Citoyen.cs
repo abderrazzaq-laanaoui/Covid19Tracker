a4305bc2c4427192647c0d880b6a2561a0742bfc
abderrezzaq laanoui <laanoui.fst@uhp.ac.ma>
2021-01-07 13:13:55 +0100

    Adding Regions

--- Citoyen.cs
+++ Citoyen.cs
@@ -21,6 +21,21 @@ namespace Covid19Track
         Vaccine,
         Decede
     }
+    public enum Regions
+    {
+        Tangier_Tetouan_AlHociema,
+        Oriental,
+        Fez_Meknes,
+        Rabat_Sale_Kenitra,
+        BeniMellal_Khenifra,
+        CasaBlanca_Settat,
+        Marrakesh_Safi,
+        Draa_Tafilalt,
+        Sous_Massa,
+        Guelmim_OuedNoun,
+        Laayoune_SakiaElHamra,
+        Dakhla_OuedEddahab
+    }
 
     public class Citoyen
     {
@@ -29,6 +44,7 @@ namespace Covid19Track
         public string nom;
         public String prenom;
         public DateTime dateDeNaissance;
+        public Regions Region { get; }
         public Etats Etat { get; set; }
         public byte DosesInjectee { get; set; }
         // -------------------------------------------- //
@@ -38,7 +54,7 @@ namespace Covid19Track
         public List<Rencontre> Rencontres { get; set; }
 
         //ctor
-        public Citoyen(string cin, string nom, string prenom, string dateDeNaissance)
+        public Citoyen(string cin, string nom, string prenom, string dateDeNaissance, Regions region)
         {
             this.CIN = cin;
             this.nom = nom;
@@ -46,6 +62,7 @@ namespace Covid19Track
             this.dateDeNaissance = DateTime.ParseExact(dateDeNaissance, "dd/MM/yyyy", CultureInfo.InvariantCulture);
             this.Etat = Etats.Inconnu;
             DosesInjectee = 0;
+            Region = region;
 
             Tests = new List<Test>();
             Records = new List<Record>();
@@ -53,19 +70,40 @@ namespace Covid19Track
             Rencontres = new List<Rencontre>();
         }
 
-        public Citoyen(string cin, string nom, string prenom, string dateDeNaissance, Etats etat)
+        public Citoyen(string cin, string nom, string prenom, string dateDeNaissance, Regions region, Etats etat)
         {
             this.CIN = cin;
             this.nom = nom;
             this.prenom = prenom;
             this.dateDeNaissance = DateTime.ParseExact(dateDeNaissance, "dd/MM/yyyy", CultureInfo.InvariantCulture);
             this.Etat = etat;
+            Region = region;
+
 
             Tests = new List<Test>();
             Records = new List<Record>();
             Isolations = new List<Isolation>();
             Rencontres = new List<Rencontre>();
         }
+        public Citoyen(string cin, string nom, string prenom, string dateDeNaissance, Etats etat, byte doses, Regions region)
+        {
+            this.CIN = cin.ToUpper();
+            this.nom = nom.ToUpper();
+            this.prenom = prenom.ToUpper();
+            this.dateDeNaissance = DateTime.ParseExact(dateDeNaissance, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+            this.Etat = etat;
+            DosesInjectee = doses;
+            Region = region;
+
+
+            Isolations = new List<Isolation>();
+            Rencontres = new List<Rencontre>();
+            Tests = new List<Test>();
+            Records = new List<Record>();
+
+
+        }
+
 
         // Return l'etat d'un citoyen sous forme d'une chaine des caracteres
         public string ConsultationEtat()
