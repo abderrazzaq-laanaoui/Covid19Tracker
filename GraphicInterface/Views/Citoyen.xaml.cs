674f7276f2281dcfe93da4925aa6883742d22b1a
abderrezzaq laanoui <laanoui.fst@uhp.ac.ma>
2021-01-10 21:58:54 +0100

    Adding personal info form

--- GraphicInterface/Views/Citoyen.xaml.cs
+++ GraphicInterface/Views/Citoyen.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +24,12 @@ namespace GraphicInterface.Views
         public Citoyen()
         {
             InitializeComponent();
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
         }
     }
 }
